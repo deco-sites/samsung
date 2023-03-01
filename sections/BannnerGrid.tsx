7225b6b4c9c6d9e7babc4591d7d9cee449e0f837
guitavano <tavano62@gmail.com>
2023-02-28 21:44:48 -0300

    change image to picture component

--- sections/BannnerGrid.tsx
+++ sections/BannnerGrid.tsx
@@ -1,5 +1,6 @@
 import Image from "$live/std/ui/components/Image.tsx";
 import type { Image as LiveImage } from "$live/std/ui/types/Image.ts";
+import { Picture, Source } from "$live/std/ui/components/Picture.tsx";
 
 import Button from "$components/ui/Button.tsx";
 
@@ -29,17 +30,30 @@ export default function BannnerGrid({
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-4 text-center">
         {images.map(({ href, src, alt, title, text, cta }) => (
           <a href={href} class="flex flex-col justify-center items-center">
-            <Image
-              width={280}
-              height={200}
-              src={src}
-              alt={alt}
-              decoding="async"
-              loading="lazy"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              class="w-full rounded-[20px] mb-2"
-            />
-            <h3 class="text-[20px] font-bold mb-2">{title}</h3>
+            <Picture class="inset-0" preload>
+              <Source
+                media="(max-width: 767px)"
+                src={src}
+                width={456}
+                height={325}
+                fetchPriority="high"
+              />
+              <Source
+                media="(min-width: 768px)"
+                src={src}
+                width={680}
+                height={485}
+                fetchPriority="high"
+              />
+              <img
+                class="object-cover w-full rounded-[20px]"
+                src={src}
+                alt={alt}
+                loading="eager"
+              />
+            </Picture>
+
+            <h3 class="text-[20px] font-bold mb-2 mt-2">{title}</h3>
             <p class="max-w-[350px] text-[14px] min-h-[80px]">{text}</p>
             <Button>{cta}</Button>
           </a>
